a24bf0897d629f2c3c5f0ec9a71d9c35a723b4ec
Mario Iuliano <mario.iuliano@visable.com>
2021-09-25 19:59:40 +0200

    new_insights

--- python/EDA/eda_munchen.py
+++ python/EDA/eda_munchen.py
@@ -2,26 +2,52 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import xlim
+import seaborn as sns
 
 path = '/Users/iuliano/Documents/projects/listings.csv'
 
 df = pd.read_csv(path)
 
 # Check the df shape
-df.shape
-df.info
+print(df.shape)
+print(df.info)
 
-# Price columns has no missing values, let's explore it
-df['price'].describe()
+# First observation goes to the 'price' dtype --> obj. Investigating why and convert it into float
+print(df['price'].sample(10))
 # Dtype is obj, some values carry the dollar sign which will need to be dropped and converted to int
 df['price'] = df['price'].str.replace('$', '')
 df['price'] = df['price'].str.replace(',', '')
 df['price'] = df['price'].astype(float)
 df['price'].describe()
 
+# Percentage of nulls in columns
+df2 = df[[c for c in df if df[c].count() / len(df) >= 0.3]]
+# Deleting columns where share of null values is up to 30% of all column data
+print("List of dropped columns", end=" ")
+for c in df:
+    if c not in df2:
+        print(c, end=',')
+df = df2
+
+# In this section I will only focus on the listing status excluding traffic data
+print(df2.columns)
+df = df[['listing_url', 'neighbourhood_cleansed', 'latitude', 'longitude',
+         'longitude', 'room_type', 'accommodates', 'bathrooms_text', 'bedrooms',
+         'beds', 'price']]
+
+# I will investigate the price distribution to have a general idea of listings prices in the city
+print(df['price'].describe())
+
+# Observations: prices with 0 values will be considered as corrupted data. Analysing the impact on the dataset
+zero_price = df['price'][df['price'] == 0].value_counts()
+# Only 3 listings: will drop those observations
+df = df.drop(df[df.price == 0].index)
+
 # One listing has 11999$ price which seems too high for an air bnb. I'm assuming that this might be data quality issue.
 # As only 1 listing has this price, I will drop the row
 df = df.drop(df[df.price == 11999].index)
+print(df['price'].describe())
+
 
 # Plot price distribution
 _ = plt.boxplot(df['price'])
@@ -40,4 +66,3 @@ outliers = df[df.price > upper_whisker]
 outliers.id.count()
 outliers.id.count() / len(df)
 # 7% of the listings fall into the outliers' region
-
