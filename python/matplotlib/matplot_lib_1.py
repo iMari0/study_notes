162e509a6b3fbde20148b666d08682cb9d4f142f
Mario Iuliano <mario.iuliano@visable.com>
2021-10-03 15:31:05 +0200

    plt

--- python/matplotlib/matplot_lib_1.py
+++ python/matplotlib/matplot_lib_1.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import random
-from datetime import timedelta
+from datetime import date, timedelta
+import matplotlib.pyplot as plt
 
 
-def cal(last_date, len_days):
+def cal(len_days, int1=1, int2=20, last_date=date.today()):
     """Function returns len_days days and a random number assigned to it
-    last_date: the date from which len_days days will be created
-    len_days: len of the df"""
+    len_days: len of the df
+    int1: minimum integer from which random int will be generated
+    int2: maximum integer until which random int will be generated
+    int1 and int2 define the range of integers from which random numbers will
+    generated
+    last_date: the date from which len_days days will be created. Defaults to current date
+    """
     global df
-    last_date = pd.to_datetime('2021-01-1', format='%Y-%m-%d')
+    last_date = pd.to_datetime(last_date, format='%Y-%m-%d')
     dates = []
     counter = 0
     while counter <= len_days:
@@ -16,6 +22,18 @@ def cal(last_date, len_days):
         dates.append(d)
         counter += 1
     dates = [d.strftime('%Y-%m-%d') for d in dates]
-    metric = [random.randint(1, 50) for x in range(len(dates))]
+    metric = [random.randint(int1, int2) for x in range(len(dates))]
     df = pd.DataFrame({'date': dates, 'metric': metric})
     return df
+
+
+df = cal(50)
+
+fig, ax = plt.subplots()
+ax.plot(df['date'],
+        df['metric'],
+        marker='o', linestyle='--', color='r')
+plt.show()
+
+
+#https://campus.datacamp.com/courses/introduction-to-data-visualization-with-matplotlib/introduction-to-matplotlib?ex=4
