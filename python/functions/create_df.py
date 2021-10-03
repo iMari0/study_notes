adb9d90b16931e1d8e1a018d5f199966fb59cce0
Mario Iuliano <mario.iuliano@visable.com>
2021-10-03 15:13:01 +0200

    create df

--- python/functions/create_df.py
+++ python/functions/create_df.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import random
-from datetime import timedelta
+from datetime import date, timedelta
 
 
-def cal(last_date, len_days, int1=1, int2=20):
+def cal(len_days, int1=1, int2=20, last_date=date.today()):
     """Function returns len_days days and a random number assigned to it
-    last_date: the date from which len_days days will be created
     len_days: len of the df
     int1: minimum integer from which random int will be generated
     int2: maximum integer until which random int will be generated
     int1 and int2 define the range of integers from which random numbers will
-    generated"""
+    generated
+    last_date: the date from which len_days days will be created. Defaults to current date
+    """
     global df
-    last_date = pd.to_datetime('2021-01-1', format='%Y-%m-%d')
+    last_date = pd.to_datetime(last_date, format='%Y-%m-%d')
     dates = []
     counter = 0
     while counter <= len_days:
